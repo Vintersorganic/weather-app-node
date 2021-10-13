41005b115768d5809ef12f8560d2993162c9072d
Jorge Gorostegui Di Peto <jorge.gdp@gmail.com>
2021-10-13 13:02:50 -0300

    small changes

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -19,7 +19,8 @@ const forecast = (lat, long, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature + " and it feels like " + body.current.feelslike)
+            callback(undefined, body.current.weather_descriptions[0] + ". It is currently " + body.current.temperature +
+             " and it feels like " + body.current.feelslike + ". The humidity is " + body.current.humidity + "%.")
         }
 })
 }
